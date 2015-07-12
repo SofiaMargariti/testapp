0546ac7e65924e53253b9893529b31c5a88d1dae
Sofia Margariti <sofiamargariti@hotmail.com>
2015-07-12 20:37:01 +0300

    Create user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150302143423) do
+ActiveRecord::Schema.define(version: 20150707161507) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.boolean  "admin"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "vessels", force: :cascade do |t|
     t.string   "title",                               null: false
